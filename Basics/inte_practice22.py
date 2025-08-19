7d31635695abf17da86f21a2b5c6f359f4e0ead0
pshelake9561 <pshelake9561@gmail.com>
2025-08-19 16:13:46 +0530

    added febonacci series program

---
+++ Basics/inte_practice22.py
@@ -0,0 +1,14 @@
+
+
+
+def febo_series(n):
+    a, b = 0, 1
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+
+
+
+num = int(input("Enter a number: "))
+febo_series(num)
